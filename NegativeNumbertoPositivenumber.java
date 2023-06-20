de527c513d10ac7f02b6b9316afcec7b545b046d
pandiancts <81268665+pandiancts@users.noreply.github.com>
2023-06-20 19:59:37 +0530

    Add files via upload

---
+++ NegativeNumbertoPositivenumber.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class NegativeNumbertoPositivenumber {
+
+	public static void main(String[] args) {
+int num = -40;
+		
+		if (num>0) {
+		System.out.println("Positive number:" +num);
+		}
+		
+		else {
+			System.out.println("Negative number:" +num);
+			
+		num = (-num);
+		}
+		{
+		System.out.println("Convert into positive number:" +num);
+		}
+		}
+	}
+		
+	
+
+
---
+++ Numberispositiveornegative.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class Numberispositiveornegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num = -40;
+		
+		if (num<0) {
+		System.out.println("Negative number:" +num);
+		}
+		
+		else {
+			System.out.println("Positive number:" +num);
+			
+		}
+	}
+
+}
